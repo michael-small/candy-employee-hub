9bbf37646bb7dd888dbbfd75adc072f3d99cd4ca
michael-small <small203@morris.umn.edu>
2020-11-17 13:39:51 -0600

    Add harcoded supervisors

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  supervisors: any[] = [
+    {
+      "name": "Doug Dayshift",
+      "outside_line": "218-346-8001",
+      "internal_ext": "2001",
+      "img": "https://cdn.pixabay.com/photo/2015/01/22/15/13/businessman-607834_960_720.png"
+    },
+    {
+      "name": "Mary Midshift",
+      "outside_line": "218-346-8002",
+      "internal_ext": "2002",
+      "img": "https://cdn.pixabay.com/photo/2014/05/31/23/16/teacher-359311_960_720.png"
+    },
+    {
+      "name": "Nate Nightshift",
+      "outside_line": "218-346-8003",
+      "internal_ext": "2003",
+      "img": "https://cdn.pixabay.com/photo/2015/01/22/15/13/businessman-607834_960_720.png"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
