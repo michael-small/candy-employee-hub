dfff0e2f1b73f413efb135419889115df82afe6c
michael-small <small203@morris.umn.edu>
2020-11-24 19:12:22 -0600

    Create auth service with hardcoded `isAdmin: true`, toggle on nav
    At some point or another, I have to start considering user roles.
    Until I impliment OAuth, this hardcoded value will have to do.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EmployeeAssignmentBoardComponent } from './employee-assignment-board/employee-assignment-board.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import {MatCardModule} from '@angular/material/card';
-import { EmployeeAssignmentBoardComponent } from './employee-assignment-board/employee-assignment-board.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -28,7 +30,9 @@ import { EmployeeAssignmentBoardComponent } from './employee-assignment-board/em
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatSlideToggleModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
