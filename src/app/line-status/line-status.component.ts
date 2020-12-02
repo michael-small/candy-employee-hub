34053cbd1fbc275970d9497fddefcfa3802f6d06
michael-small <small203@morris.umn.edu>
2020-12-02 00:44:44 -0600

    Sort line statuses shallowly by past/present/future in ngOnInit
    Statuses that have been set as of component initialization, aka
    the hardcoded ones, are filtered from the `lineStatusArray` into
    past/present/future LineStatus arrays by date. However, new
    entries are not sorted into those categories as of form
    submission, so they don't show up in the view. That is out of
    scope for this commit.
    
    In a similair sort of vein, the hardcoded statuses were listed
    in an order that would look as if the system knows how to order
    shifts in a "proper" chronological order. As in:
    * Filtering doesn't sort days within a category, like Dec 1 of
    the past and Dec 2 of the past.
    * Filtering doesn't assume some order of night>day>mid.
    * Filtering doesn't order statuses in a single shift on a single
    day, like trial running @ noon and then cleaning at 1pm.
    
    Some of those examples are loaded with assumptions that need to
    be discussed with the customers, like what the order of shifts
    are in a "day". IE is a day structured traditionally like
    day>mid>night, or practically night>day>mid like many days workout
    as I assumed. It is because of these assumptions that no logic is
    set in place as of yet until customer feedback, though the
    seed data will be ordered in such a way based on assumed behaivor.
    If it seems easy to impliment and trash, some code may sneak its
    way in.
    
    Addresses PR #6 (Create "Line Status" page)

--- src/app/line-status/line-status.component.ts
+++ src/app/line-status/line-status.component.ts
@@ -26,6 +26,10 @@ interface LineStatus {
 })
 export class LineStatusComponent implements OnInit {
 
+  public archivedStatuses: LineStatus[];
+  public futureStatuses: LineStatus[];
+  public todaysStatuses: LineStatus[];
+
   public users: User[];
 
   lineStatusForm = new FormGroup({
@@ -36,8 +40,15 @@ export class LineStatusComponent implements OnInit {
   });
 
   lineStatusArray: LineStatus[] = [
+    {comment: '', status: 'Cleaning', date: 'Wed Nov 01 2020 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Day'},
+    {comment: '', status: 'Cleaning', date: 'Wed Nov 02 2020 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Mid'},
+    {comment: '<DEMO NOTE: 12-1-2019. We need to discuss how to handle displaying years.>', status: 'Cleaning', date: 'Wed Dec 01 2019 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Night'},
+    {comment: '', status: 'Cleaning', date: new Date().toDateString(), shift: 'Mid'},
+    {comment: 'Active clogged up the mixer.', status: 'Cleaning', date: new Date().toDateString(), shift: 'Day'},
+    {comment: 'Going good as of noon.', status: 'Trial', date: new Date().toDateString(), shift: 'Day'},
+    {comment: '', status: 'Down', date: new Date().toDateString(), shift: 'Night'},
     {comment: 'Active is caked on hard. Will require caustic.', status: 'Cleaning', date: 'Wed Dec 09 2020 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Mid'},
-    {comment: '', status: 'Cleaning', date: 'Wed Dec 01 2020 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Mid'},
+    {comment: '<DEMO NOTE: 12-2-2025. We need to discuss how to handle displaying years.>', status: 'Cleaning', date: 'Wed Dec 02 2025 00:00:00 GMT-0600 (Central Standard Time)', shift: 'Night'},
   ];
 
   lineStatusInstance: LineStatus = ({comment: '', status: '', date: '', shift: ''});
@@ -67,6 +78,15 @@ export class LineStatusComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    // for splitting the archived line statuses into past/present/future
+    const today = new Date();
+    today.setHours(0, 0 , 0, 0);
+
+    this.archivedStatuses = this.lineStatusArray.filter(status => Date.parse(status.date) < today.getTime());
+    this.futureStatuses = this.lineStatusArray.filter(status => Date.parse(status.date) > today.getTime());
+    this.todaysStatuses = this.lineStatusArray.filter(status => Date.parse(status.date).valueOf() === today.getTime().valueOf());
+
     this.authService.getUsers()
       .subscribe(users => {
         this.users = users;
