e9965f57df5d8d4a4396098a139890c59b12ce9b
michael-small <small203@morris.umn.edu>
2020-12-03 11:53:57 -0600

    Import mat-expansion-panel, group `app.module.ts imports`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,34 @@
+// Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EmployeeAssignmentBoardComponent } from './employee-assignment-board/employee-assignment-board.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
-import { HttpClientModule } from '@angular/common/http';
-
-import {MatSelect, MatSelectModule} from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
-
-
-
-
 import { LineStatusComponent } from './line-status/line-status.component';
 
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -63,6 +59,7 @@ import { LineStatusComponent } from './line-status/line-status.component';
     MatNativeDateModule,
     MatIconModule,
     HttpClientModule,
+    MatExpansionModule
   ],
   providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
